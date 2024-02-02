a21525bd956ddfb6b76e02970ec3643c5f91f2e3
laurgu <lgu011@uottawa.ca>
2024-02-02 08:30:23 -0500

    multi-threaded version

--- Assignment1/Main.py
+++ Assignment1/Main.py
@@ -1,91 +1,30 @@
-# The Code Shouldn't take more than 5 Seconds to Run
-
-# regular expression (regex) module in Python. Patterns used to match character combinations in strings.
-import re
-# Natural Language Toolkit (NLTK)
-import nltk
-
 import os
-# Stopwords are like basic non-special words
+import re
+import nltk
+import time
 from nltk.corpus import stopwords
-nltk.download('stopwords')
-# Imported just in case someone wanted to use it 
 from nltk.stem import PorterStemmer
-# For parsing
-from xml.etree import ElementTree as ET
-
 from bs4 import BeautifulSoup
-
-from lxml import etree
-
-# For Dictionaries
 from collections import defaultdict
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from concurrent.futures import ProcessPoolExecutor
 
-# Part 1
-print("Part 1 running...")
-
+# nltk.download('stopwords')
 
 def preprocess(text):
-    # Remove markup that is not part of the text
     text = re.sub(r'<.*?>', '', text)
-
-    # Tokenization - Split the text into words
     tokens = re.findall(r'\b\w+\b', text.lower())
-
-    # Remove stopwords
     stop_words = set(stopwords.words('english'))
     tokens = [word for word in tokens if word not in stop_words]
-
-    # Don't use Porter Stemmer, it cuts some words off incorrectly, (Cut officers to offic)
-    # Using Porter does feel to speed up the code 
-    # porter = PorterStemmer()
-    # tokens = [porter.stem(word) for word in tokens]
-
     return tokens
 
-# Part 2
-print("Part 2 running...")
-# Creates the inverted index for 1 document
-
-
-def buildInvertedIndex(doc_id, tokens):
-    inverted_index = {}
-
-    # For every token(word)    
-    for position, token in enumerate(tokens):
-        # Add the token to inverted index / position
-        if token not in inverted_index:
-            inverted_index[token] = [(doc_id, position)]
-        else:
-            inverted_index[token].append((doc_id, position))
-
-    return inverted_index
-
-directory = './coll'  
-
-# Use a list to store results
-all_results = []
-
-for filename in os.listdir(directory):
-    filepath = os.path.join(directory, filename)
-
-    # Parse the XML data from the file
-    # I needed to slightly modify the documents the professor gave us cause I kept running into errors with her code, please use the SampleDocument.xml file for right now
+def process_document(filepath):
     tree = BeautifulSoup(open(filepath, 'r').read().replace("\n", ""), 'lxml')
 
-    # Get the root
-
-    # If multiple parents, wrap in another tag as root
-
-    # Use a defaultdict to store results with DocID as key and processed text as values
     doc_data = defaultdict(set)
 
-    # Process each DOC element
     for doc in tree('doc'):
-        # Find the docid and text
         doc_id = doc.find('docno').string.strip()
 
         try:
@@ -93,50 +32,77 @@ for filename in os.listdir(directory):
         except AttributeError:
             text = ""
 
-        # Preprocess the text and store in the set
         processed_text = preprocess(text)
         doc_data[doc_id] = processed_text
 
-    # Global Inverted Index    
+    return doc_data
+
+def build_inverted_index(doc_id, tokens):
     inverted_index = {}
-    # For every Doc
-    for doc_id, tokens in doc_data.items():
-        doc_index = buildInvertedIndex(doc_id, tokens)
-        # Merge doc_index into inverted_index
-        for token, postings in doc_index.items():
-            if token not in inverted_index:
-                inverted_index[token] = postings
-            else:
-                inverted_index[token].extend(postings)
 
-# Part 3 Goes Here
-print("Part 3 running...")
+    for position, token in enumerate(tokens):
+        if token not in inverted_index:
+            inverted_index[token] = [(doc_id, position)]
+        else:
+            inverted_index[token].append((doc_id, position))
 
-query = preprocess("Accusations of Cheating by Contractors on U.S. Defense Projects")
+    return inverted_index
 
-# Convert the doc_data values (processed text) to a list
-all_documents = [" ".join(doc) for doc in doc_data.values()]
+def perform_search(query, doc_data_list):
+    inverted_index = {}
+    all_documents = []
 
-# Create a TF-IDF Vectorizer
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(all_documents)
+    for doc_data in doc_data_list:
+        for doc_id, tokens in doc_data.items():
+            doc_index = build_inverted_index(doc_id, tokens)
 
-# Convert the query to a TF-IDF vector
-query_vector = vectorizer.transform([" ".join(query)])
+            for token, postings in doc_index.items():
+                if token not in inverted_index:
+                    inverted_index[token] = postings
+                else:
+                    inverted_index[token].extend(postings)
 
-# Compute cosine similarities between the query and documents
-cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
+        # Convert the doc_data values (processed text) to a list
+        all_documents.extend([" ".join(doc) for doc in doc_data.values()])
 
-# Sort documents based on cosine similarity
-sorted_documents = sorted(zip(doc_data.keys(), cosine_similarities), key=lambda x: x[1], reverse=True)
+    # Create a TF-IDF Vectorizer
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(all_documents)
 
-# Append the sorted documents to the results list
-for doc_id, score in sorted_documents:
-    all_results.append(f"Document: {doc_id}, Cosine Similarity: {score:.4f}")
+    query_vector = vectorizer.transform([" ".join(query)])
 
-# Write the results to a text file
-with open('Results.txt', 'w') as file:
-    for result in all_results:
-        file.write(result + '\n')
+    cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
 
-print("Results written to Results.txt")
+    sorted_documents = sorted(zip(doc_data.keys(), cosine_similarities), key=lambda x: x[1], reverse=True)
+
+    return sorted_documents
+
+def write_results(sorted_documents, output_file='Results.txt'):
+    all_results = []
+    query_number = 0
+    counter = 1
+
+    for doc_id, score in sorted_documents:
+        all_results.append(f"{query_number} , Q0 , {doc_id} {counter} {score:.4f}, run_name")
+        counter += 1
+
+    with open(output_file, 'w') as file:
+        for result in all_results:
+            file.write(result + '\n')
+
+def main():
+    start = time.time()
+    directory = './coll'
+
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_document, os.path.join(directory, filename)) for filename in os.listdir(directory)]
+        doc_data_list = [future.result() for future in futures]
+
+    sorted_documents = perform_search("Accusations of Cheating by Contractors on U.S. Defense Projects", doc_data_list)
+
+    write_results(sorted_documents)
+
+    print("--- %s seconds ---" % (time.time() - start))
+
+if __name__ == "__main__":
+    main()
