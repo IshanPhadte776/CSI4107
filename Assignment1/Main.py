f78126f69c98920eca0bfbaef970de8fe5f79453
laurgu <lgu011@uottawa.ca>
2024-02-07 15:51:45 -0500

    working?

--- Assignment1/Main.py
+++ Assignment1/Main.py
@@ -1,17 +1,13 @@
-import os
 import re
-import nltk
-import time
+import os
+import concurrent.futures
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from concurrent.futures import ProcessPoolExecutor
 
-# nltk.download('stopwords')
-
+# Part 1: Preprocessing
 def preprocess(text):
     text = re.sub(r'<.*?>', '', text)
     tokens = re.findall(r'\b\w+\b', text.lower())
@@ -19,90 +15,108 @@ def preprocess(text):
     tokens = [word for word in tokens if word not in stop_words]
     return tokens
 
-def process_document(filepath):
-    tree = BeautifulSoup(open(filepath, 'r').read().replace("\n", ""), 'lxml')
-
-    doc_data = defaultdict(set)
-
-    for doc in tree('doc'):
-        doc_id = doc.find('docno').string.strip()
-
-        try:
-            text = doc.find('text').string.strip()
-        except AttributeError:
-            text = ""
-
-        processed_text = preprocess(text)
-        doc_data[doc_id] = processed_text
-
-    return doc_data
-
+# Part 2: Build Inverted Index for a Document
 def build_inverted_index(doc_id, tokens):
     inverted_index = {}
-
     for position, token in enumerate(tokens):
         if token not in inverted_index:
             inverted_index[token] = [(doc_id, position)]
         else:
             inverted_index[token].append((doc_id, position))
-
     return inverted_index
 
-def perform_search(query, doc_data_list):
+# Part 3: Process Documents and Build Global Inverted Index
+def process_document(filepath):
+    tree = BeautifulSoup(open(filepath, 'r').read().replace("\n", ""), 'lxml')
+    
+    doc_data = defaultdict(list)
+    
+    for doc in tree('doc'):
+        doc_id = doc.find('docno').string.strip()
+        
+        try:
+            text = doc.find('text').string.strip()
+        except AttributeError:
+            text = ""
+        
+        processed_text = preprocess(text)
+        doc_data[doc_id] = processed_text
+    
+    return doc_data
+
+def process_documents(directory):
+    all_results = []
+    doc_data = defaultdict(list)
+    filepaths = [os.path.join(directory, filename) for filename in os.listdir(directory)]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(process_document, filepaths))
+
+    for result in results:
+        for doc_id, tokens in result.items():
+            doc_data[doc_id] = tokens
+
     inverted_index = {}
-    all_documents = []
+    for doc_id, tokens in doc_data.items():
+        doc_index = build_inverted_index(doc_id, tokens)
+        for token, postings in doc_index.items():
+            if token not in inverted_index:
+                inverted_index[token] = postings
+            else:
+                inverted_index[token].extend(postings)
 
-    for doc_data in doc_data_list:
-        for doc_id, tokens in doc_data.items():
-            doc_index = build_inverted_index(doc_id, tokens)
+    return doc_data, inverted_index
 
-            for token, postings in doc_index.items():
-                if token not in inverted_index:
-                    inverted_index[token] = postings
-                else:
-                    inverted_index[token].extend(postings)
+def perform_search(query, doc_data, inverted_index):
+    query = " ".join(query)
+    
+    # Process the query using the same preprocessing as the documents
+    processed_query = preprocess(query)
 
-        # Convert the doc_data values (processed text) to a list
-        all_documents.extend([" ".join(doc) for doc in doc_data.values()])
+    relevant_docs = set()
+    
+    # Retrieve relevant documents from the inverted index
+    for term in processed_query:
+        if term in inverted_index:
+            relevant_docs.update([posting[0] for posting in inverted_index[term]])
 
-    # Create a TF-IDF Vectorizer
+    # Convert relevant documents to a list for further processing
+    relevant_docs = list(relevant_docs)
+
+    # Prepare data for the TF-IDF vectorizer
+    all_documents = [" ".join(doc_data[doc_id]) for doc_id in relevant_docs]
+    
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(all_documents)
-
-    query_vector = vectorizer.transform([" ".join(query)])
-
+    query_vector = vectorizer.transform([query])
+    
+    # Calculate cosine similarities only for relevant documents
     cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
-
-    sorted_documents = sorted(zip(doc_data.keys(), cosine_similarities), key=lambda x: x[1], reverse=True)
-
-    return sorted_documents
-
-def write_results(sorted_documents, output_file='Results.txt'):
-    all_results = []
+    
+    # Sort and write results to 'Results.txt'
+    sorted_documents = sorted(zip(relevant_docs, cosine_similarities), key=lambda x: x[1], reverse=True)
+    
+    counter = 0
     query_number = 0
-    counter = 1
-
+    all_results = []
+    
     for doc_id, score in sorted_documents:
         all_results.append(f"{query_number} , Q0 , {doc_id} {counter} {score:.4f}, run_name")
         counter += 1
 
-    with open(output_file, 'w') as file:
+    with open('Results.txt', 'w') as file:
         for result in all_results:
             file.write(result + '\n')
 
+# Main Function
 def main():
-    start = time.time()
-    directory = './coll'
 
-    with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_document, os.path.join(directory, filename)) for filename in os.listdir(directory)]
-        doc_data_list = [future.result() for future in futures]
-
-    sorted_documents = perform_search("Accusations of Cheating by Contractors on U.S. Defense Projects", doc_data_list)
-
-    write_results(sorted_documents)
-
-    print("--- %s seconds ---" % (time.time() - start))
+    directory = './coll' 
+    doc_data, inverted_index = process_documents(directory)
+    
+    query = preprocess("Accusations of Cheating by Contractors on U.S. Defense Projects")
+    
+    perform_search(query, doc_data, inverted_index)
 
 if __name__ == "__main__":
     main()
